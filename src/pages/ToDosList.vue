d49c3d37d60aafde317885ffbaa60e7b8a15443f
Jason Baisden <jasonbaisden@gmail.com>
2020-07-02 09:59:48 -0500

    Added todos, delete todo, filter todo

--- src/pages/ToDosList.vue
+++ src/pages/ToDosList.vue
@@ -1,16 +1,24 @@
 // FROM TUTORIAL AT https://www.youtube.com/watch?v=5lVQgZzLMHc&t=1513s
 <template>
   <div>
+    <AddTodo></AddTodo>
+    <FilterTodos />
     <todos-comp> </todos-comp>  
   </div>
 </template>
 
 <script>
 import TodosComp from '../components/ToDos'
+import AddTodo from '../components/AddTodo'
+import FilterTodos from '../components/FilterTodos'
+
+import { mapActions } from 'vuex'
 export default {
     name: "ToDosList",
     components: {
-      TodosComp
+      TodosComp,
+      FilterTodos,
+      AddTodo
     }
 }
 </script>
